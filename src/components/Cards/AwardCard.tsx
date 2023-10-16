bb8b9782975507b83e7d8709985f109c44a5ca8a
0xUkhezo <ukhezo.web3@gmail.com>
2023-10-16 12:49:35 +0200

    feat(styles): add nex animations

--- src/components/Cards/AwardCard.tsx
+++ src/components/Cards/AwardCard.tsx
@@ -13,25 +13,29 @@ interface AwardCardProps {
 export default function AwardCard({ award, index }: AwardCardProps) {
   return (
     <div
-      className={`flex flex-row my-10 mx-auto  ${index === 0 && "hiddenShowLeft"} ${
-        index === 1 && "hiddenShowRight"
-      } ${
+      className={`my-10 mx-auto min-w-[300px] md:min-w-[500px] ${
+        index === 0 && "hiddenShowLeft"
+      } ${index === 1 && "hiddenShowRight"} ${
         index === 2 && "lg:col-span-2 hiddenShowUp"
       } border-1 border-black items-center rounded-xl bg-gray-900 hover:shadow-pink hover:shadow-xl hover:border-pink`}
     >
-      <a className="flex px-10 py-6" href={award.href} target="_blank">
+      <a
+        className="flex md:flex-row flex-col items-center px-10 py-6"
+        href={award.href}
+        target="_blank"
+      >
         <Image
           src={award.image}
           alt={`Image 1`}
           height={144}
           width={144}
-          className={`max-h-[144px] max-w-[144px] ${index % 2 !== 0 ? "hidden" : "block"}`}
+          className={`max-h-[144px] max-w-[144px] ${index % 2 !== 0 ? "lg:hidden" : "block"}`}
         />
-        <div className={`text-start ${index % 2 === 0 ? "ml-20" : "mr-20"}`}>
-          <h2 className="mb-6 text-2xl font-anton tracking-widest">{award.title}</h2>
-          <div>
-            <h3 className="font-bitter text-2xl">Winner by:</h3>
-            <div className="ml-8 mt-4">
+        <div className={`text-start ${index % 2 === 0 ? "md:ml-20" : "md:mr-20"}`}>
+          <h2 className="mb-6 text-2xl font-anton tracking-widest mt-8 lg:mt-0">{award.title}</h2>
+          <div className="text-center md:text-start">
+            <h3 className="font-bitter text-2xl">Winner in:</h3>
+            <div className="md:ml-8 mt-4">
               {award.winners.map((winner: any) => {
                 return <div className="my-2 font-bitter text-xl">{winner}</div>
               })}
@@ -43,7 +47,9 @@ export default function AwardCard({ award, index }: AwardCardProps) {
           alt={`Image 1`}
           height={144}
           width={144}
-          className={`max-h-[144px] max-w-[144px] ${index % 2 === 0 ? "hidden" : "block"}`}
+          className={`max-h-[144px] max-w-[144px] ${
+            index % 2 === 0 ? "hidden" : "lg:block hidden"
+          }`}
         />
       </a>
     </div>
--- src/components/Cards/ProjectCard.tsx
+++ src/components/Cards/ProjectCard.tsx
@@ -33,11 +33,11 @@ export default function ProjectCard({ index, project }: ProjectCardProps) {
   }, [])
 
   return (
-    <a target="_blank" href={project.url}>
+    <a target="_blank" href={project.url} className="hiddenShowUp">
       <section
         className={`flex text-center rounded-xl ${
           index % 2 !== 1 ? "lg:pr-10" : "lg:pl-10"
-        } lg:my-20 py-5 lg:max-h-[500px] flex lg:flex-row flex-col cardProjectStart hover:shadow-pink hover:shadow-xl hover:border-pink`}
+        } lg:my-20 py-5 my-10 lg:my-0 lg:max-h-[500px] flex lg:flex-row flex-col cardProjectStart hover:shadow-pink hover:shadow-xl hover:border-pink`}
       >
         <Image
           src={Logo.src}
@@ -46,7 +46,7 @@ export default function ProjectCard({ index, project }: ProjectCardProps) {
           height={400}
           className={
             index % 2 !== 1
-              ? "projectImageStart xl:max-w-[400px] xl:max-h-[400px] lg:max-w-[300px] lg:max-h-[300px] rounded-lg hidden lg:block"
+              ? "projectImageStart xl:max-w-[400px] xl:max-h-[400px] lg:max-w-[300px] lg:max-h-[300px] rounded-lg hidden lg:block "
               : "hidden"
           }
         />
@@ -59,7 +59,7 @@ export default function ProjectCard({ index, project }: ProjectCardProps) {
             "projectImageStart xl:max-w-[400px] xl:max-h-[400px] max-w-[250px] max-h-[250px] rounded-lg lg:hidden block "
           }
         />
-        <div className="text-white lg:text-start my-auto projectTextStart lg:max-h-[400px] mt-4 ">
+        <div className="text-white lg:text-start my-auto projectTextStart lg:max-h-[400px] mt-10 lg:mt-4">
           <h1 className="font-semibold text-4xl mb-4 font-anton tracking-wider">
             {project.title}
           </h1>
