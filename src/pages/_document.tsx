607760b9fd383c8199df5b7814de44cfc79dc07e
0xUkhezo <ukhezo.web3@gmail.com>
2023-10-16 13:14:53 +0200

    feat(favicon): add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,5 @@
 // Next
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
@@ -10,5 +10,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
