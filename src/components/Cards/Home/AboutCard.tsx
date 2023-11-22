005d2e46a7fef47f16de422452eada42ae5d8ed6
0xUkhezo <ukhezo.web3@gmail.com>
2023-11-22 11:11:23 +0100

    feat(styles): add hover effect in about card

--- src/components/Cards/Home/AboutCard.tsx
+++ src/components/Cards/Home/AboutCard.tsx
@@ -7,31 +7,19 @@ import Star from "../../../../public/Star.svg"
 export default function AboutCard() {
   const [isHovered, setIsHovered] = useState(false)
 
-  useEffect(() => {
-    const card = document.getElementById("cardAbout")
-    if (isHovered) {
-      card?.classList.add("card-about-hovered")
-    } else {
-      card?.classList.remove("card-about-hovered")
-    }
-  }, [isHovered])
-
   return (
-    <Link href="/about" id="cardAbout">
+    <Link href="/about" className="card-about ">
       <div
-        className="rounded-3xl bg-gradient-to-tl from-[#3f3f3f] to-[#141414] flex "
+        className="rounded-3xl bg-gradient-to-tl from-[#3f3f3f] to-[#141414] flex justify-center w-full"
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
       >
-        <div className="flex flex-col md:flex-row justify-between gap-x-[36px] items-center m-8 w-full">
-          <Image
-            height={224}
-            width={224}
-            src={isHovered ? Logo.src : Logo.src}
-            alt="Alvaro Teran Image"
-            className="md:rounded-tl-3xl md:rounded-br-3xl "
-          />
-          <div className="relative w-full mt-6 md:mt-0 ">
+        <div className="relative flex flex-col md:flex-row gap-x-[36px] items-center m-8 w-full">
+          <article>
+            <img src={Logo.src} alt="Alvaro Teran Image" />
+            <img src={Logo.src} alt="Alvaro Teran Image" />
+          </article>
+          <div className=" w-fit mt-6 md:mt-0 ">
             <h2 className="mb-1.5 font-semibold text-xl opacity-transition-show show">
               {isHovered ? "Web3 Developer" : "Front End Developer"}
             </h2>
@@ -52,17 +40,17 @@ export default function AboutCard() {
                 <span>I am a Front End Developer based in Madrid.</span>
               )}
             </p>
-            <Image
-              height={46}
-              width={42}
-              src={Star.src}
-              id="star"
-              alt="Alvaro Teran Image"
-              className={`absolute right-0 bottom-0 md:-bottom-7 ${
-                isHovered ? "opacity-100" : "opacity-40"
-              } transition-opacity duration-300`}
-            />
           </div>
+          <Image
+            height={46}
+            width={42}
+            src={Star.src}
+            id="star"
+            alt="Alvaro Teran Image"
+            className={`absolute right-0 bottom-0 md:bottom-1${
+              isHovered ? "opacity-100" : "opacity-40 "
+            } transition-opacity duration-300`}
+          />
         </div>
       </div>
     </Link>
