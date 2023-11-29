05758ed61000a90f5d76ca3a6031b10ec9c5023f
0xUkhezo <ukhezo.web3@gmail.com>
2023-11-29 13:39:53 +0100

    feat(styles): add Kusanagi font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 // Next
 import type { AppProps } from "next/app"
+import local from "next/font/local"
 // Styles
 import "@/styles/globals.css"
 
+const myFont = local({
+  src: "../../public/fonts/Kusanagi.otf",
+  display: "swap",
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={myFont.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
